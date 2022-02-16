d882ba6ebb7efa5474420c208d2c8e0b9e243229
jongsoo.bae <js.bae@heumlabs.io>
2022-02-17 02:18:14 +0900

    echo server

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from 'express'
+
 const app = express()
 const port = process.env.port || 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello world!')
+app.use(express.json())
+
+app.post('/', (req, res) => {
+  res.send(req.body)
 })
 
 app.listen(port, () => {
